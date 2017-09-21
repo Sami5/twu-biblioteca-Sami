abb42e1b502891b4bc798ebd61465f3e9b006b16
Sami <sml_4000@hotmail.com>
2017-09-21 18:01:48 +1000

    Refactor existing books instantiation

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -4,21 +4,21 @@ import java.util.List;
 
 public class BibliotecaApp {
 
+    public static void existingBooks() {
+        Book book1 = new Book("Game of Thrones", "Bob", 1900);
+        Book book2 = new Book("Lord of the Rings", "John", 1950);
+        Book book3 = new Book("Harry Potter", "Victoria", 1857);
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Biblioteca");
 
-        Book book1 = new Book("Game of Thrones", "Bob", 1900);
-        Book book2 = new Book("Lord of the Rings", "John", 1950);
-        Book book3 = new Book("Harry Potter", "Victoria", 1857);
-/*
-        for(Book x: ListBooks.bookList) {
-            System.out.println(x);
-        }
-*/
+        MainMenu.getAllItems();
+
+        existingBooks();
 
         ListBooks.getAllBooks();
-        MainMenu.getAllItems();
 
     }
 }
